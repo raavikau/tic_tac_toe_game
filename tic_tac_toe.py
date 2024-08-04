2162deb033f0128173f698b7041447083e2af822
Raavi Kaur <raavi211299@gmail.com>
2024-08-04 16:26:51 -0500

    add code to check winning conditions and who wins or tie

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,13 +6,21 @@ def draw_board(spot):
     board = f"|{spot[1]}|{spot[2]}|{spot[3]}|\n|{spot[4]}|{spot[5]}|{spot[6]}|\n|{spot[7]}|{spot[8]}|{spot[9]}|"
     print(board)
 
-def check_turn(turn):
-    if turn % 2 == 0:
+def check_turn(player_turn):
+    if player_turn % 2 == 0:
         return 'X'
     else:
         return 'O'
-
-playing = True
+def check_for_win(spot):
+    if (spot[1] == spot[2] == spot[3]) or (spot[4] == spot[5] == spot[6]) or (spot[7] == spot[8] == spot[9]):
+        return True
+    elif (spot[1] == spot[4] == spot[7]) or (spot[2] == spot[5] == spot[8]) or (spot[3] == spot[6] == spot[9]):
+        return True
+    elif (spot[1] == spot[5] == spot[9]) or (spot[3] == spot[5] == spot[7]):
+        return True
+    else:
+        return False
+playing, complete = True, False
 turn = 0
 prev_turn = -1
 while playing:
@@ -21,11 +29,26 @@ while playing:
     if prev_turn == turn:
         print("Spot already fill, please pick other")
     prev_turn = turn
-    print("Player ", str((turn % 2)+1), "'s turn: pick your spot")
+    print("Player ", str((turn % 2)+1), "'s turn: pick your spot or press 'q' to quit")
     choice = input()
-    if choice == 'q':
+    if choice.lower() == 'q':
         playing = False
     elif str.isdigit(choice) and int(choice) in spots:
         if not spots[int(choice)] in {'X', 'O'}:
             turn += 1
             spots[int(choice)] = check_turn(turn)
+    if turn > 8:
+        playing = False
+    if check_for_win(spots):
+        playing = False
+        complete = True
+os.system('cls' if os.name == 'nt' else 'clear')
+draw_board(spots)
+if complete:
+    print("Game Over")
+    if check_turn(turn) == 'O':
+        print("**** Player 1 winner ****")
+    else:
+        print("**** Player 2 winner ****")
+else:
+    print("No Winner")
